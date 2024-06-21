3a35e9d61d1fbda7408b3a1ebc8efbaad9c8114d
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-06-21 15:10:27 +0530

    jsonserver

--- classstatepropsexample/src/components/JSONserverAPI.jsx
+++ classstatepropsexample/src/components/JSONserverAPI.jsx
@@ -1,13 +1,22 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const JSONserverAPI = () => {
     const [data,setData] = useState({
         name:"",
         age:'',
-        salary:''
+        salary:'',
+        id:''
     })
+  
     const [userData,setUserData] = useState([])
+    const fetchData = ()=>{
+      axios.get('http://localhost:4000/users')
+      .then((json)=>setUserData(json.data))
+    }
+    useEffect(()=>{
+      fetchData()
+    },[])
     const handleChange = (e)=>{
         const {name,value} = e.target
         setData({
@@ -23,6 +32,32 @@ const JSONserverAPI = () => {
         axios.post('http://localhost:4000/users',data)
         .then((json)=>console.log(json))
     }
+    const editData = (id)=>{
+        axios.patch('http://localhost:4000/users/'+id)
+        .then((json)=>{
+          setData({
+            name:json.data.name,
+            age:json.data.age,
+            salary:json.data.salary,
+            id:json.data.id
+
+          })
+        })
+
+    }
+    const updateData = ()=>{
+      let id = data.id
+      axios.put('http://localhost:4000/users/'+id,data)
+      .then((json)=>console.log(json.data))
+
+      fetchData()
+      setData({
+        name:"",
+        age:'',
+        salary:'',
+        id:''
+    })
+    }
   return (
     <div>
       <h3>Json Server CRUD Example</h3>
@@ -30,8 +65,31 @@ const JSONserverAPI = () => {
       Name: <input type="text" name="name" id="" value={data.name} onChange={handleChange}/>
       Age: <input type="text" name="age" id="" value={data.age} onChange={handleChange}/>
       Salary: <input type="text" name="salary" id="" value={data.salary} onChange={handleChange}/>
-       <input type="button" name="save" id="" value="Save" onClick={saveData}/>
+       <input type="button" name="save" id="" value={ data.id ? "Update":"Save"} onClick={data.id ? updateData : saveData}/>
       </form>
+
+      <table>
+        <tr>
+          <td>Id</td>
+          <td>Name</td>
+          <td>Age</td>
+          <td>Salary</td>
+          <td>Action</td>
+        </tr>
+        {
+          userData && userData.map((i)=>{
+              return (
+                <tr>
+                  <td>{i.id}</td>
+                  <td>{i.name}</td>
+                  <td>{i.age}</td>
+                  <td>{i.salary}</td>
+                  <td><button onClick={()=>editData(i.id)}>Edit</button></td>
+                </tr>
+              )
+          })
+        }
+      </table>
     </div>
   )
 }
